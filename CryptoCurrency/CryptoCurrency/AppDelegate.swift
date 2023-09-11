07abdab7650efd94db952cac294dd93279bc6fdb
Hoang Linh <hlinh.jobs@gmail.com>
2023-09-11 17:28:26 +0700

    Init project

---
+++ CryptoCurrency/CryptoCurrency/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  CryptoCurrency
+//
+//  Created by Hoang Linh Nguyen on 11/9/2023.
+//
+
+import UIKit
+import CoreData
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
