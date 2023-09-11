7b9441211eb6362be2d29c5c6d451be38f9cbe5f
Hoang Linh <hlinh.jobs@gmail.com>
2023-09-11 17:28:26 +0700

    Init project

---
+++ CryptoCurrency/CryptoCurrency/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  CryptoCurrency
+//
+//  Created by Hoang Linh Nguyen on 11/9/2023.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
---
+++ CryptoCurrency/CryptoCurrency/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CryptoCurrency
+//
+//  Created by Hoang Linh Nguyen on 11/9/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
