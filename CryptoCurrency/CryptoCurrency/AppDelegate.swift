7ec73171954c45d154e4bb50ecc878701a6cb920
Hoang Linh <hlinh.jobs@gmail.com>
2023-09-12 13:16:32 +0700

    Build Bottom Tab Bar

--- CryptoCurrency/CryptoCurrency/AppDelegate.swift
+++ CryptoCurrency/CryptoCurrency/AppDelegate.swift
@@ -10,8 +10,12 @@ import CoreData
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 }
