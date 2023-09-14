dd81101ccf9414d114cf95af499ee03e10f59ba5
Hoang Linh <hlinh.jobs@gmail.com>
2023-09-14 15:33:26 +0700

    Build Favourite Screen UI

---
+++ CryptoCurrency/CryptoCurrency/Extension/ViewExtension.swift
@@ -0,0 +1,34 @@
+//
+//  ViewExtension.swift
+//  CryptoCurrency
+//
+//  Created by Hoang Linh Nguyen on 14/9/2023.
+//
+
+import UIKit
+
+extension UIView {
+    func roundTopLeftCorner() {
+        self.layer.cornerRadius = 10
+        self.layer.maskedCorners = .layerMinXMinYCorner
+        self.layer.masksToBounds = true
+    }
+
+    func roundTopRightCorner() {
+        self.layer.cornerRadius = 10
+        self.layer.maskedCorners = .layerMaxXMinYCorner
+        self.layer.masksToBounds = true
+    }
+
+    func roundBottomLeftCorner() {
+        self.layer.cornerRadius = 10
+        self.layer.maskedCorners = .layerMinXMaxYCorner
+        self.layer.masksToBounds = true
+    }
+
+    func roundBottomRightCorner() {
+        self.layer.cornerRadius = 10
+        self.layer.maskedCorners = .layerMaxXMaxYCorner
+        self.layer.masksToBounds = true
+    }
+}
